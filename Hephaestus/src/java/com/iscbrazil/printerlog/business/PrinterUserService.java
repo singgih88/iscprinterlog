d19b75c9643a3ab1012e2c79e35862cd47f71b12
sela-roinuj <sela-roinuj@hotmail.com>
2011-04-13 13:01:15 +0000

    PrinterUser search, clickable table and Printer datatable working. Implementing classes from com.iscbrazil.printerlog.business

--- Hephaestus/src/java/com/iscbrazil/printerlog/business/PrinterUserService.java
+++ Hephaestus/src/java/com/iscbrazil/printerlog/business/PrinterUserService.java
@@ -1,11 +1,23 @@
 package com.iscbrazil.printerlog.business;
 
+import com.iscbrazil.printerlog.DAO.FactoryDAO;
+import com.iscbrazil.printerlog.DAO.PrinterUserDAO;
+import com.iscbrazil.printerlog.model.PrinterUser;
+import java.util.List;
+
 /**
  * @version 2011.APR.12.01
  * @author edilson.ales
  */
 public class PrinterUserService {
 
-    
+    public List<PrinterUser> getAll() {
+
+        FactoryDAO factory = FactoryDAO.getFactoryDAO();
+        PrinterUserDAO printerUserDAO = factory.getPrinterUserDAO();
+        List<PrinterUser> users = printerUserDAO.getAll();
+        factory.shutTx();
+        return users;
+    }
 
 }
