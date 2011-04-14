e18ac7f598fa8473638c545b43dbd25891c3e0f2
sela-roinuj <sela-roinuj@hotmail.com>
2011-04-14 20:49:28 +0000

    With IE is working fileupload on admin.xhtml. Other browsers are required

--- Hephaestus/src/java/com/iscbrazil/printerlog/managedBeans/AdminBean.java
+++ Hephaestus/src/java/com/iscbrazil/printerlog/managedBeans/AdminBean.java
@@ -1,38 +1,54 @@
 package com.iscbrazil.printerlog.managedBeans;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 /**
- * @version 2011.APR.13.01
+ * @version 2011.APR.14.01
  * @author edilson.ales
  */
 @ManagedBean
-@RequestScoped
-public class AdminBean {
+@SessionScoped
+public class AdminBean implements Serializable {
 
-    public void aefhaeth() throws IOException {
-        String filePath = "C:/Users/edilson.ales/Documents/2008.06.23.txt";
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader(filePath));
-            System.out.println(in.readLine());
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(AdminBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
+    private String lastFileUploaded;
+    private List<String> lines;
 
     public void handleFileUpload(FileUploadEvent event) {
         UploadedFile file = event.getFile();
-        System.out.println(file.getFileName());
+        this.lastFileUploaded = file.getFileName();
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(file.getInputstream()));
+            this.lastFileUploaded = in.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(AdminBean.class.getName()).log(Level.SEVERE, null, ex);
+        }        
     }
+
+    public String getLastFileUploaded() {
+        return lastFileUploaded;
+    }
+
+    public void setLastFileUploaded(String lastFileUploaded) {
+        this.lastFileUploaded = lastFileUploaded;
+    }
+
+    public List<String> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<String> lines) {
+        this.lines = lines;
+    }
+
 }
