c61c44315eaceb7a691c893fa057adc1bfb738be
sela-roinuj <sela-roinuj@hotmail.com>
2011-04-19 19:55:50 +0000

    Text file upload persisting perfectly on IE. Other Browsers are required.

--- Hephaestus/src/java/com/iscbrazil/printerlog/DAO/hibernate/HibernatePrinterUserDAO.java
+++ Hephaestus/src/java/com/iscbrazil/printerlog/DAO/hibernate/HibernatePrinterUserDAO.java
@@ -2,10 +2,12 @@ package com.iscbrazil.printerlog.DAO.hibernate;
 
 import com.iscbrazil.printerlog.DAO.PrinterUserDAO;
 import com.iscbrazil.printerlog.model.PrinterUser;
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
- * @version 2011.APR.12.01
+ * @version 2011.APR.19.01
  * @author edilson.ales
  */
 public class HibernatePrinterUserDAO extends HibernateGenericDAO<PrinterUser> implements PrinterUserDAO {
@@ -13,11 +15,21 @@ public class HibernatePrinterUserDAO extends HibernateGenericDAO<PrinterUser> im
     public HibernatePrinterUserDAO(Session session) {
         super(session);
     }
-
+    
     @Override
     public PrinterUser getByLogin(String login) {
-        return (PrinterUser) this.getSession().get(PrinterUser.class, "login");
+        Query query = this.getSession().createQuery("from PrinterUser where login = :userLogin");
+        query.setParameter("userLogin", login);
+        List users = query.list();
+        PrinterUser u = new PrinterUser();
+        try {
+            for (Object aux : users) {
+                u = (PrinterUser) aux;
+            }
+            return u;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }   
     }
-
-
 }
